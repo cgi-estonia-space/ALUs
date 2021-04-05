23ac4c05317f2cbb829d9a14746272541b3555d9
Karl Tõnissoo <karl.tonissoo@cgi.com>
2021-04-05 20:21:23 +0300

    snapgpu-231 - fixed pre-binned multiband histogram
    * changed logic to display multiple pre-binned histograms
    * modified import path for gdal_calc
    * added some wrapper classes to improve readability
    * joined legend and band statistics and moved these under the chart
    * pre-binning made by GDAL should be more accurate now
    * added missing dependencies to requirements.txt

--- build-automation/compare_alus_output.py
+++ build-automation/compare_alus_output.py
@@ -3,59 +3,107 @@
 import argparse
 import math
 import sys
+import os
 from pathlib import Path
 
 import matplotlib.pyplot as plt
 import numpy as np
 from osgeo import gdal
 
-# There is no official way to import gdal_calc.py therefore use the one installed in system executable dir.
-sys.path.append("/usr/bin/")
+# There is no official way to import gdal_calc.py therefore use the one in environment created from requirements.txt
+sys.path.append(os.path.join(Path(sys.executable).parent, ''))
 from gdal_calc import *
 
 
-def produce_tif_statistics(tif_file, figure_nr, do_save, short_label=''):
+class GDALBandStatsWrapper:
+    """
+    Converts GDAL's band.GetStatistics(0, 1)
+    to friendly plottable object for matplotlib
+    """
+
+    def __init__(self, gdal_band, name):
+        self.min = gdal_band[0]
+        self.max = gdal_band[1]
+        self.avg = gdal_band[2]
+        self.std = gdal_band[3]
+        self.name = name
+
+
+class GDALHistogramWrapper:
+    """
+    Converts GDAL's band.GetHistogram(min=min_value, max=max_value, buckets=bucket_count, approx_ok=False)
+    to friendly plottable object for matplotlib
+    """
+
+    def __init__(self, gdal_band_histogram, gdal_bucket_count, gdal_min_value, gdal_max_value):
+        self.raw_histogram = np.asarray(gdal_band_histogram)
+        self.bucket_count = gdal_bucket_count
+        self.min_value = gdal_min_value
+        self.max_value = gdal_max_value
+
+    def bucket_edges(self):
+        return np.linspace(self.min_value, self.max_value, self.bucket_count + 1)
+
+    def bucket_centroids(self):
+        return (self.bucket_edges()[1:] + self.bucket_edges()[:-1]) / 2
+
+    def plotting_weights(self):
+        return self.raw_histogram
+
+    def plotting_bins(self):
+        return self.bucket_count
+
+    def plotting_range_min(self):
+        return min(self.bucket_edges())
+
+    def plotting_range_max(self):
+        return max(self.bucket_edges())
+
+
+def produce_tif_statistics(tif_file, name, is_multiband):
     ds = gdal.Open(tif_file)
     band = ds.GetRasterBand(1)
-    (band_min, band_max, band_avg, band_std) = band.GetStatistics(0, 1)
+    band_stats = GDALBandStatsWrapper(band.GetStatistics(0, 1), name)
     bucket_count = 256
-    raw_hist = band.GetHistogram(min=band_min, max=band_max, buckets=bucket_count)
-    bin_edges = np.linspace(band_min, band_max, bucket_count + 1)
+    if is_multiband:
+        # since we pre-bin it using GDAL, we need to use exactly same binning for both datasets
+        min_value = 0
+        max_value = 1
+        band_histogram = GDALHistogramWrapper(
+            band.GetHistogram(min=min_value, max=max_value, buckets=bucket_count, approx_ok=False), bucket_count,
+            min_value, max_value)
+    else:
+        band_histogram = GDALHistogramWrapper(
+            band.GetHistogram(min=band_stats.min, max=band_stats.max, buckets=bucket_count, approx_ok=False),
+            bucket_count, band_stats.min, band_stats.max)
+    return band_stats, band_histogram
 
-    bins = np.array(bin_edges)
-    counts = np.array(raw_hist)
-    assert len(bins) == len(counts) + 1
 
-    centroids = (bins[1:] + bins[:-1]) / 2
+def plot_histogram(do_save, figure_nr, histograms, tif_file, band_stats):
     plt.figure(figure_nr)
-    plt.hist(centroids, bins=len(counts), weights=counts, range=(min(bins), max(bins)), label=short_label)
-    plt.legend(bbox_to_anchor=(1.05, 1), loc='upper left', borderaxespad=0.)
-
+    plt.ticklabel_format(style='plain')
+    # if multiple histograms are provided, make sure they have been binned similarly
+    plt.hist([h.bucket_centroids() for h in histograms], bins=histograms[0].bucket_count,
+             weights=[h.plotting_weights() for h in histograms], range=(
+            min([h.plotting_range_min() for h in histograms]), max([h.plotting_range_max() for h in histograms])),
+             label=[b.name + ' ' + 'min: ' + str(b.min) + ' ' + 'max: ' + str(b.max) + ' ' + 'mean: ' + str(
+                 b.avg) + ' ' + 'std dev: ' + str(b.std) for b in band_stats])
+    plt.legend(bbox_to_anchor=(0.5, -0.2), loc='lower center', borderaxespad=0.)
     plt.xlabel('pixel values', size=14)
     plt.ylabel('value occurrence', size=14)
-    plt.title('Histogram\n' + Path(tif_file).stem, size=16, va='bottom', ha='center')
-    statistical_text = short_label + '\n' + \
-                       'min: ' + str(band_min) + '\n' + \
-                       'max: ' + str(band_max) + '\n' + \
-                       'mean: ' + str(band_avg) + '\n' + \
-                       'std dev: ' + str(band_std)
-    plt.text(band_avg + band_std, ((max(counts) - min(counts)) / 4) * 3, statistical_text, size='12')
-
+    plt.title('Histogram\n' + Path(tif_file).stem, size=12, va='bottom', ha='center')
     fig = plt.gcf()
     dpi = fig.get_dpi()
     fig.set_size_inches(1024.0 / float(dpi), 800.0 / float(dpi))
-
     if do_save:
         filename = '/tmp/' + Path(tif_file).stem + ".pdf"
         plt.savefig(filename, bbox_inches='tight', pad_inches=0.5)
         print("Saved histogram - " + filename)
 
-    return band_min, band_max, band_avg, band_std
-
 
 def log_statistics(filename, statistic):
     response = {filename.format(1): "min=%.5f, max=%.5f, mean=%.5f, std_dev=%.5f" % (
-        statistic[0], statistic[1], statistic[2], statistic[3])}
+        statistic.min, statistic.max, statistic.avg, statistic.std)}
     print(response)
 
 
@@ -75,12 +123,16 @@ def main():
     # Identical to CLI call - gdal_calc.py -A "golden_file.tif" -B "compare_file.tif" --calc="A-B" --outfile=...
     Calc('A-B', A=args.golden_file, B=args.compare_file, outfile=diff_file)
     print("Saved diff file - " + diff_file)
-    golden_file_stats = produce_tif_statistics(args.golden_file, 1, False, 'SNAP')
+
+    (golden_file_stats, histo1) = produce_tif_statistics(args.golden_file, 'SNAP', True)
     log_statistics(args.golden_file, golden_file_stats)
-    compare_file_stats = produce_tif_statistics(args.compare_file, 1, True, 'ALUS')
+    (compare_file_stats, histo2) = produce_tif_statistics(args.compare_file, 'ALUS', True)
     log_statistics(args.compare_file, compare_file_stats)
-    diff_file_stats = produce_tif_statistics(diff_file, 2, True, '<SNAP pixel> - <ALUS pixel>')
+    plot_histogram(True, 1, [histo1, histo2], args.compare_file, [golden_file_stats, compare_file_stats])
+
+    (diff_file_stats, histo) = produce_tif_statistics(diff_file, '<SNAP pixel> - <ALUS pixel>', False)
     log_statistics(diff_file, diff_file_stats)
+    plot_histogram(True, 2, [histo], diff_file, [diff_file_stats])
 
     transform_ok = True
     if args.check_transform:
