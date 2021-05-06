9d974f90d9fab4f74392f565f076cef769d7c11e
Sven Kautlenbach <sven.kautlenbach@cgi.com>
2021-05-06 12:14:49 +0300

    alus-249 Nightly build scripts.

---
+++ build-automation/execute_nightly.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+set -e
+
+function print_help {
+    echo "Usage:"
+    echo "$0 <build ID> <test data folder> <dem files location>"
+}
+
+if [[ "$#" != 3 ]]; then
+    echo "Wrong count of input arguments"
+    print_help
+    exit 1
+fi
+
+build_id=$1
+
+tar -xzvf ${build_id}.tar.gz
+
+# Run scripts of different cases
+mv run_beirut_disaster_test.sh ${build_id}
+
+cd ${build_id}
+
+# Run cases
+./run_beirut_disaster_test.sh $2 $3
+
+cd ..
+
+aws s3api put-object --bucket alus-builds --key "${build_id}/${build_id}.tar.gz" --body ${build_id}.tar.gz --acl public-read --storage-class STANDARD_IA --profile tarmo
+echo "Uploaded binary package available at https://alus-builds.s3.eu-central-1.amazonaws.com/${build_id}/${build_id}.tar.gz"
+
+cd /tmp
+for file in *.tif; do
+        aws s3api put-object --bucket alus-builds --key "${build_id}/${file}" --body $file --acl public-read --storage-class STANDARD_IA --profile tarmo
+        echo "Uploaded resource available at https://alus-builds.s3.eu-central-1.amazonaws.com/${build_id}/${file}"
+done
+
+for file in *tc.tif; do
+        png_file=${file%.*}.png
+        gdal_translate -of PNG -ot Byte -scale $file $png_file
+        aws s3api put-object --bucket alus-builds --key "${build_id}/${png_file}" --body $png_file --acl public-read --storage-class STANDARD_IA --profile tarmo
+        echo "Uploaded resource available at https://alus-builds.s3.eu-central-1.amazonaws.com/${build_id}/${png_file}"
+done
