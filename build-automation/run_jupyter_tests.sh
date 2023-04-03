a54b6f9b00eb59d64f92b4f6b353e4725247cf5c
Sven Kautlenbach <sven.kautlenbach@cgi.com>
2023-04-03 15:12:54 +0300

    E2E tests fix.

--- build-automation/run_jupyter_tests.sh
+++ build-automation/run_jupyter_tests.sh
@@ -61,7 +61,7 @@ ls -lah ..
 "$original_folder"/alus_result_check.py -I "$calibration_output_file" -G "$NIGHTLY_GOLDEN_DIR"/S1A_IW_SLC__1SDV_20210722T005537_20210722T005604_038883_049695_2E58_tnr_Cal_IW2_deb_tc_jupyter.tif
 res2=$?
 
-coherence_output_file="$output_dir/S1A_IW_SLC__1SDV_20200724T034334_20200724T034401_033591_03E49D_96AA_Orb_Stack_coh_deb_tc.tif"
+coherence_output_file="$output_dir/S1A_IW_SLC__1SDV_20200724T034334_20200724T034401_033591_03E49D_96AA_Orb_Stack_IW1_coh_deb_tc.tif"
 echo "Validating $coherence_output_file"
 "$original_folder"/alus_result_check.py -I "$coherence_output_file" -G "$NIGHTLY_GOLDEN_DIR"/S1A_IW_SLC__1SDV_20200724T034334_20200724T034401_033591_03E49D_96AA_Orb_Stack_coh_deb_TC_jupyter.tif
 res3=$?
